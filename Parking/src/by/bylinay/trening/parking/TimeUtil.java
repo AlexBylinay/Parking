18088f3840f1996ba458410f65fe17ced000f42d
giga <giga@DESKTOP-FBL23JC>
2021-04-13 22:37:06 +0300

    pre-final

---
+++ Parking/src/by/bylinay/trening/parking/TimeUtil.java
@@ -0,0 +1,20 @@
+package by.bylinay.trening.parking;
+ 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+ 
+public class TimeUtil {
+    
+    private static final DateFormat FORMAT = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
+    // TODO разобратьс€ как это работает, и подготовитьс€ это рассказать и объ€снить
+    public static synchronized String format (Date date) {
+        return FORMAT.format(date);
+    }
+    public static synchronized Date parse (String formattedDate) throws ParseException{
+        return FORMAT.parse(formattedDate);
+    }
+ 
+}
